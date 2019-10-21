238ecd5492ee97475e0ec45cff9a86cb18562430
tiola1 <aleksandrakrezel5@gmail.com>
2019-10-21 19:50:46 +0200

    zmiany w skryptach

---
+++ napisy.py
@@ -0,0 +1,12 @@
+#
+#Typ danych napisy (albo string)
+#
+napis1 = "Ala ma kota"
+napis2 = 'Ala ma kota'
+napis3 = 'A to jest "cudzysłów"'
+napis4 = "A to jest \"cudzysłów\" "
+napis5 = "Znaki specjalne: \t \n \r "
+
+długosc = len(napis1)
+print("Długosc zmiennej napis1 to ",długosc, "znaków")
+
