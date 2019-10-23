cc0d2403a0469b9e67498d7843ec58735f41b56a
tiola1 <aleksandrakrezel5@gmail.com>
2019-10-23 18:21:37 +0200

    dokonczenie konfiguracji

---
+++ arytmetyka.py
@@ -0,0 +1,15 @@
+# 1. przypisz do zmiennych x i y jakies wartosci liczbowe 
+#korzystając z funkcji print wypisz na ekranie podstawowe działania arytmetyczne (dodawanie, odejmowanie, dzilenie, mnożenie, dzielenie całkowite, reszta z dzielenia), potęgowanie 
+#uruchom program kilka razy dla różnych wartosci x i y 
+
+x = 10
+y = 15
+
+print("x=", x, "y=", y) # wywolanie print
+print("dodawanie:", x + y)
+print("dzielenie:", x/y)
+print("dzielenie całkowite:", x // y)
+print("odejmowanie:", x - y)
+print("potęgowanie:", x**y)
+print("dzielenie modulo:", x %y)
+#ctrl + alt + l
---
+++ typy liczbowe.py
@@ -0,0 +1,32 @@
+# w Pythonie mamy:
+# liczby całkowite - integer
+print(type(10))
+print(int())
+print(int"10"))
+print(int("10", base=3))
+print(int(2.7))
+
+# liczby zmiennoprzecinkowe -float
+print(float())
+print(float(1/3))
+print(0.1 + 0.1 + 0.1)
+print (0.1 + 0.1 + 0.1 == 0.3)
+print(round(1.23, 2))
+
+#notacja naukowa
+print
+
+# liczby zespolone - = complex
+print (complex())
+print(10 +11j)
+
+#wartosci boolowskie: bool True False
+print(10 + True)
+# print (10/False)
+# or and not
+
+# Prześledzić działanie operatorów logicznych or and not
+print (True and False)
+print (True or False)
+
+
