2d5125878d080e18b9368431ec1664d3a486c7a2
tiola1 <aleksandrakrezel5@gmail.com>
2020-02-03 20:21:28 +0100

    ostatnie zajecia

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from polls.views import index
+from polls.views import index, question_details, question_list, vote
+
+app_name = "polls"
 
 urlpatterns = [
     path("", index, name="index"),
-
+    path('pytanie/', question_list, name="questions-list"),
+    path('pytanie/<id>', question_details, name="details")
+    path('pytanie/<id>/vote', vote, name="vote")
 ]
+
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,49 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
+from django.urls import reverse
 # Create your views here.
+from django.urls import reverse
+
+from polls.models import Question
 
 
 def index(request):
     return HttpResponse("Hello world! That's polls index")
 
 
-def
+# def questions_list(request):
+
+
+def question_details(request, id):
+    q = Question.objects.get(id=id)
+
+    return render(
+        request,
+        "details.html",
+        {"question": q}
+    )
+
+
+def question_list(request):
+    qs = Question.objects.get(id=id)
+
+    return render(
+        request,
+        "list.html",
+        {"questions": qs}
+    )
+
+def vote(request, id):
+    question = Question.objects.get(pk=id)
+    choice_id=request.POST.get('choice')
+    if choice_id:
+        choice = question.choices.get(id=choice_id)
+        choice.votes += 1
+        choice.save()
+    else:
+        return render(request, "details.html,"
+                               " {"question":question"
+        {}
+    print("Odbyło się głosowanie")
+
+    return HttpResponseRedirect(reverse"")
